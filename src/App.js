46fd87f381c8203fbea5dc7deff2263ac07b71f8
Angel Estevez <angelestevez@MacBook-Air-de-Angel.local>
2023-10-08 00:45:37 -0600

    estilos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+import React from 'react';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
-import './App.css';
+
+const defaultTodos = [
+  {text: 'pendiente 1', completed: true},
+  {text: 'pendiente 2', completed: true},
+  {text: 'pendiente 3', completed: false}
+];
 
 function App() {
   return (
-    <div className="App">
-      <TodoCounter/>
+    <>
+      <TodoCounter completed={15} total={20}/>
       <TodoSearch/>
       <TodoList>
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
+        {defaultTodos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
+        ))}
       </TodoList>
       <CreateTodoButton/>
-    </div>
+    </>
   );
 }
 
